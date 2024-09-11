aadb3e064c9a9e380ec54fe95bafdb1ac6e5161d
Rishik7570 <rishiksingha@gmail.com>
2024-09-11 07:08:41 +0530

    frontend completed

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,8 +1,47 @@
+import { assets } from "../assets/assets"
+import NewsLetter from "../components/NewsLetter"
+import Title from "../components/Title"
 
 
 const About = () => {
+
   return (
     <div>
+
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1="ABOUT" text2="US"/>
+      </div>
+
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img src={assets.about_img} alt="" className="w-full md:max-w-[450px]" />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda maiores explicabo libero sequi voluptatem dolor nihil vero pariatur? Nulla officiis et ab quo aut dolor facere debitis esse natus distinctio!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel reiciendis itaque nemo aliquam temporibus, fuga ut praesentium quaerat consequatur cum minus ratione, commodi placeat eius nisi ducimus dolorem, esse id.</p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quae obcaecati alias aut dolorum, vero dolores, accusantium voluptatibus saepe veritatis aliquid sapiente vitae eius, laborum consequuntur quasi suscipit aspernatur expedita!</p>
+        </div>
+      </div>
+
+      <div className="text-xl py-4">
+        <Title text1="WHY" text2="CHOOSE US"/>
+      </div>
+
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance</b>
+          <p className="text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, eius nulla! Quod</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convinience</b>
+          <p className="text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, eius nulla! Quod</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service</b>
+          <p className="text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, eius nulla! Quod</p>
+        </div>
+      </div>
+
+      <NewsLetter />
       
     </div>
   )
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,37 @@
+import { useState } from "react"
 
 
 const Login = () => {
+
+  const [currState,setCurrState] = useState('Login')
+
+  const onsubmithandler = async(e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+  }
+
   return (
-    <div>
+    <form onSubmit={onsubmithandler} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">{currState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-700" />
+      </div>
+
+      {currState === 'Login' ? '' : <input type="text" className="w-full px-3 py-2 border border-gray-800" placeholder="Name" required/>}
       
-    </div>
+      <input type="email" className="w-full px-3 py-2 border border-gray-800" placeholder="Email" required/>
+      <input type="password" className="w-full px-3 py-2 border border-gray-800" placeholder="Password" required/>
+      <div className="w-full flex justify-between text-sm mt-[-8px]">
+
+        <p className="">Forgot your password?</p>
+        {
+          currState === 'Login'
+          ? <p onClick={()=>setCurrState('Sign Up')} className="cursor-pointer" >Create account</p>
+          : <p onClick={()=>setCurrState('Login')} className="cursor-pointer" >Login here</p>
+        }
+
+      </div>
+      <button type="submit" className="bg-black text-white font-light px-8 py-2 mt-4">{currState === 'Login' ? 'Sign in' : 'Sign Up'}</button>
+    </form>
   )
 }
 
